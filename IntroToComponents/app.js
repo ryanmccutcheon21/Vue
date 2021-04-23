57928a6cb4e2cdb9f82c3d2f13230d625d4134ad
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-23 18:54:21 -0500

    Intro to Components

--- IntroToComponents/app.js
+++ IntroToComponents/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: 'Kayla',
@@ -17,10 +16,36 @@ const app = Vue.createApp({
                 },
             ]
         }
+    }
+})
+
+app.component('friend-contact', {
+    template: `
+    <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'Kayla',
+                name: 'Kayla McCutcheon',
+                phone: '9187065018',
+                email: 'kayla@localhost.com',
+            },
+        }
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible
         }
     }
-}).mount('#app')
+})
+
+app.mount('#app')
