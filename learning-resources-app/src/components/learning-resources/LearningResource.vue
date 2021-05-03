81eb95d514c0fba650fffe1d87e1ea638b2e5e17
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-03 18:10:48 -0500

    Added BaseButton

--- learning-resources-app/src/components/learning-resources/LearningResource.vue
+++ learning-resources-app/src/components/learning-resources/LearningResource.vue
@@ -1,15 +1,15 @@
 <template>
   <li>
-    <BaseCard>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
         <a :href="link">View Resource</a>
       </nav>
-    </BaseCard>
+    </base-card>
   </li>
 </template>
 
