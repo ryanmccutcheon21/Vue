10f9cc9785ae01c9f6670cf8dbfccab9a6d726fd
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-03 14:54:06 -0500

    Initial Project/Component setup

---
+++ learning-resources-app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from "./components/learning-resources/LearningResource.vue";
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js Documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.org",
+        },
+      ],
+    };
+  },
+};
+</script>
