75b759d82951323af602efb74ea2ce5fd6fd9c07
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-05 15:42:33 -0500

    Added delete resources functionality

--- learning-resources-app/src/components/learning-resources/TheResource.vue
+++ learning-resources-app/src/components/learning-resources/TheResource.vue
@@ -51,6 +51,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      removeResource: this.removeResource,
     };
   },
   computed: {
@@ -74,6 +75,12 @@ export default {
       };
       this.storedResources.unshift(newResource);
       this.selectedTab = "stored-resources";
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
     },
   },
 };
