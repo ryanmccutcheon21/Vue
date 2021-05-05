d66f4bedfe51f20113bf46166c37b8a3930c0848
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-05 14:59:43 -0500

    Fetching user input

--- learning-resources-app/src/components/learning-resources/AddResource.vue
+++ learning-resources-app/src/components/learning-resources/AddResource.vue
@@ -1,9 +1,9 @@
 <template>
   <base-card>
-    <form action="">
+    <form action="" @submit.prevent="submitData">
       <div class="form-control">
         <label for="title">Title</label>
-        <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" ref="titleInput" />
       </div>
       <div class="form-control">
         <label for="description">Description</label>
@@ -12,11 +12,12 @@
           id="description"
           cols="30"
           rows="3"
+          ref="descInput"
         ></textarea>
       </div>
       <div class="form-control">
         <label for="link">Link</label>
-        <input type="url" name="link" id="link" />
+        <input type="url" name="link" id="link" ref="linkInput" />
       </div>
       <div class="form-control">
         <base-button type="submit">Add Resource</base-button>
@@ -24,6 +25,22 @@
     </form>
   </base-card>
 </template>
+
+<script>
+export default {
+  inject: ["addResource"],
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredUrl = this.$refs.linkInput.value;
+
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+  },
+};
+</script>
+
 
 <style scoped>
 label {
