1273bcb2fdec80a76607f1f74ed8c80dd289d6fb
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-07 20:42:55 -0500

    Finished RatingControl component

--- Forms/src/components/RatingControl.vue
+++ Forms/src/components/RatingControl.vue
@@ -1,12 +1,42 @@
 <template>
   <ul>
-    <li><button type="button">Poor</button></li>
-    <li><button type="button">Average</button></li>
-    <li><button type="button">Great</button></li>
+    <li :class="{ active: activeOption === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: activeOption === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: activeOption === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
   </ul>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      activeOption: null,
+    };
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+    },
+  },
+};
+</script>
+
+
 <style scoped>
+.active {
+  border-color: purple;
+}
+
+.active button {
+  color: purple;
+}
+
 ul {
   list-style: none;
   margin: 0.5rem 0;
