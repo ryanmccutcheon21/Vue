d7f7cddb528cdeba18d3f3418df799b05af0c08a
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-14 18:01:51 -0500

    Programmatic Navigation

--- Routing/src/components/users/UsersList.vue
+++ Routing/src/components/users/UsersList.vue
@@ -1,7 +1,15 @@
 <template>
-  <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
-  </ul>
+  <div>
+    <button @click="confirmInput">Confirm</button>
+    <ul>
+      <user-item
+        v-for="user in users"
+        :key="user.id"
+        :name="user.fullName"
+        :role="user.role"
+      ></user-item>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -12,6 +20,12 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+      // do something
+      this.$router.push('/teams');
+    },
+  },
 };
 </script>
 
