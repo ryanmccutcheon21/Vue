894596547320970676b3f0a26d284d6b94e7ddcd
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-20 16:42:48 -0500

    Organizing Route Files

--- Routing/src/pages/UsersList.vue
+++ Routing/src/pages/UsersList.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import UserItem from './UserItem.vue';
+import UserItem from '../components/users/UserItem.vue';
 
 export default {
   components: {
---
+++ Routing/src/router.js
@@ -0,0 +1,77 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './pages/TeamsList.vue'
+import UsersList from './pages/UsersList.vue'
+import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './pages/NotFound.vue'
+import TeamsFooter from './pages/TeamsFooter.vue'
+import UsersFooter from './pages/UsersFooter.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/teams' },
+        // { path: '/teams', component: TeamsList, alias: '/' },
+        {
+            name: 'teams',
+            path: '/teams',
+            meta: { needsAuth: true },
+            components: { default: TeamsList, footer: TeamsFooter },
+            children: [
+                {
+                    name: 'team-members',
+                    path: ':teamId',
+                    component: TeamMembers,
+                    props: true
+                }, // /teams/:teamId
+            ]
+        },
+        {
+            path: '/users',
+            components: {
+                default: UsersList,
+                footer: UsersFooter
+            },
+            beforeEnter(to, from, next) {
+                console.log('users beforeEnter')
+                console.log(to, from);
+                next();
+            }
+        },
+        { path: '/:notFound(.*)', component: NotFound }
+    ],
+    linkActiveClass: 'active',
+    scrollBehavior(_, _2, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        return {
+            left: 0,
+            top: 0,
+        }
+    }
+});
+
+router.beforeEach((to, from, next) => {
+    // if (to.name === 'team-members') {
+    //     next();
+    // } else {
+    //     next({ name: 'team-members', params: { teamId: 't2' } });
+    // }
+    console.log('Global beforeEach');
+    console.log(to, from);
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!');
+        next();
+    } else {
+        next();
+    }
+    next();
+});
+
+router.afterEach((to, from) => {
+    // sending analytics data
+    console.log('Global afterEach');
+    console.log(to, from);
+})
+
+export default router;
